f217d2f27d4e0f0137b945e4992d3aee95da81cc
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-10-13 15:43:06 +0200

    corrected generation of random numbers

--- array-tail/array-tail.js
+++ array-tail/array-tail.js
@@ -1,4 +1,5 @@
-const btn = document.querySelector("button");
+const btn = document.querySelector(".btn-success");
+const btn2 = document.querySelector(".btn-info");
 const inputEl = document.getElementById("data");
 const resultEl = document.querySelector(".alert");
 
@@ -6,12 +7,14 @@ const userArray = [
 
 ]
 btn.addEventListener("click", function(){
-
+    userArray.splice(0,userArray.length)
     let userValue = inputEl.value;
     for(let i = 1; i <= userValue; i++){
-        userArray.push(i);
+        userArray.push(getRndInteger (0,100));
     }
     resultEl.innerHTML = userArray.slice(userArray.length - 5)
     resultEl.classList.remove("d-none")
+    console.log(userArray)
+
 }
 )
